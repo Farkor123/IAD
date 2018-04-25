8b538088ef8a19f4de241e4fe73255a262000860
Farkor123 <a.sadowski1@onet.eu>
2018-04-25 14:50:51 +0200

    Update program.py
    .

--- Exercise2/Program.py
+++ Exercise2/Program.py
@@ -7,7 +7,9 @@ from tqdm import tqdm
 
 from MLP import *
 from Parser import *
-
+##############
+#Parser block#
+##############
 P = Parser()
 P.parse()
 _epochs = P.get_epochs()
@@ -15,20 +17,30 @@ _lambda = P.get_lambda()
 _momentum = P.get_momentum()
 _data = P.get_dataset()
 _hidden = P.get_hidden()
-
+###########
+#CSV block#
+###########
 df = pd.read_csv(_data + '.data.txt', header = None)
 df_height, df_width = df.shape
 for i in range(df_height):
     for j in range(df_width):
         if type(df.iloc[i, j]) is str:
             df.iloc[i, j] = ord(df.iloc[i, j])
-
+print(df.iloc[:,df_width - 1])
+#############
+#Layer block#
+#############
 layer_array = np.array([])
-for i in range(_hidden[0] + 1):
-    MLP.neuronlayer()
-
-
-
+for i in range(_hidden[0]):
+    if i == 0:
+        np.append(layer_array, neuronlayer(_hidden[1], df_width - 1))
+    else:
+        np.append(layer_array, neuronlayer(hidden[i+1], _hidden[i]))
+np.append(layer_array, neuronlayer(1, _hidden[len(_hidden) - 1]))
+network = neuralnetwork(layer_array)
+############################
+#Shitty stuff, smells funny#
+############################
 ox = list()
 oy = list()
 for j in tqdm(range(_epochs)):
