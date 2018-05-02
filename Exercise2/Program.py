50ad8a6786042b1aaf5dd17febcb3e0a5e5e170a
Farkor123 <a.sadowski1@onet.eu>
2018-05-02 17:24:54 +0200

    .
    .

--- Exercise2/Program.py
+++ Exercise2/Program.py
@@ -12,20 +12,20 @@ import MLP
 np.seterr(over = 'ignore')
 P = Parser.Parser()
 P.parse()
-_epochs = P.get_epochs()
-_lambda = P.get_lambda()
-_momentum = P.get_momentum()
+_epochs = 1000
+_lambda = 0.005
+_momentum = 0.9
 _data = P.get_dataset()
 _hidden = P.get_hidden()
 #############
 # CSV block #
 #############
-df = pd.read_csv(_data + '.data.txt', header=None)
+df = pd.read_csv(_data + '.data', header=None)
 df_height, df_width = df.shape
 for i in range(df_height):
     for j in range(df_width):
         if type(df.iloc[i, j]) is str:
-            df.iloc[i, j] = ord(df.iloc[i, j])
+            df.iloc[i, j] = ord(df.iloc[i, j]) / 100
 inputs = np.asmatrix(df.as_matrix())
 input_matrix = inputs[:, :df_width - 1]
 dupa = df.iloc[:, 8].as_matrix()
@@ -34,9 +34,15 @@ df = np.asmatrix(df.as_matrix())
 dupa = np.sort(dupa)
 classes = pd.DataFrame(np.zeros([df_height, dupa.size]))
 for i in range(dupa.size):
-    classes.iloc[:, i] = df[:, -1] == dupa[i]
+    classes.iloc[:, i] = df[:, -1] >= dupa[i]
 classes = np.asmatrix(classes.as_matrix())
-output_matrix = classes
+#ch, cw = classes.shape
+#intclas = np.zeros((ch, cw))
+#for i in range(ch):
+#    for j in range(cw):
+#        intclas[i, j] = float(classes[i, j])
+#output_matrix = classes
+output_matrix = classes.astype(np.float)
 ###############
 # Layer block #
 ###############
@@ -57,8 +63,8 @@ network = MLP.neuralnetwork(layer_array)
 ox = list()
 oy = list()
 arr = list(range(0, df_height))
-for j in tqdm(range(_epochs)):
-    for x in tqdm(range(df_height)):
+for j in range(_epochs):
+    for x in range(df_height):
         network.back_propagate(
             input_matrix[arr[x]].T, output_matrix[arr[x]].T, _lambda, _momentum)
     if j % 1 == 0:
@@ -68,11 +74,13 @@ for j in tqdm(range(_epochs)):
             cost = cost + (layer_array[-1].error[q, 0]**2)
         oy.append(float(cost))
     np.random.shuffle(arr)
-
-for k in range(4):
-    network.forward_propagate(input_matrix[k].T)
-    print("\n###########################################")
-    print(output_matrix[k].T, layer_array[-1].output)
+    _lambda *= 0.997
+    perc = 0
+    for k in range(df_height):
+        network.forward_propagate(input_matrix[k].T)
+        if(np.matrix.sum(output_matrix[k]) == np.matrix.sum(layer_array[-1].output[:] > 0.5)):
+            perc += 1
+    print(100 * perc / df_height,"%\n")
 cost = 0
 for q in range(dupa.size):
     cost = cost + (layer_array[-1].error[q, 0]**2)
